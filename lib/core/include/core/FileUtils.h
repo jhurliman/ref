b3bbede2b2502f06282d41b6cb7cfc9d9c63c9d0
John Hurliman <jhurliman@jhurliman.org>
2018-10-20 12:31:50 -0700

    FileLength()

---
+++ lib/core/include/core/FileUtils.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <fstream>
+
+namespace ref {
+
+size_t FileLength(std::ifstream& file);
+
+}  // namespace ref
---
+++ lib/core/src/FileUtils.cpp
@@ -0,0 +1,13 @@
+#include "core/FileUtils.h"
+
+namespace ref {
+
+size_t FileLength(std::ifstream& file) {
+    auto pos = file.tellg();
+    file.seekg(0, std::ios::end);
+    size_t length = file.tellg();
+    file.seekg(pos);
+    return length;
+}
+
+}  // namespace ref
