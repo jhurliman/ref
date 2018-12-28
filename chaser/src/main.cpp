635176f216637f1e0a63d8501cc762502789cfd1
John Hurliman <jhurliman@jhurliman.org>
2018-12-27 22:36:50 -0800

    Use Controller in chaser

--- chaser/src/main.cpp
+++ chaser/src/main.cpp
@@ -1,3 +1,4 @@
+#include <controller/Controller.h>
 #include <core/Assert.h>
 #include <core/FileUtils.h>
 #include <core/Log.h>
@@ -12,7 +13,17 @@
 #include <time.h>
 #include <unistd.h>
 
-static void loop() {
+static std::unique_ptr<ref::NodeBase> CreateNode(const ref::NodeDefinition& def, const ref::Graph g) {
+    if (def.nodeType() == "WebcamDriver") {
+        return std::make_unique<ref::WebcamDriver>(def, g);
+    } else if (def.nodeType() == "Recorder") {
+        return std::make_unique<ref::Recorder>(def, g);
+    } else {
+        ABORT_ALWAYS(ref::Format("Unrecognized node type '%s'", def.nodeType()));
+    }
+}
+
+static void Loop() {
     using namespace ref::filesystem;
     using namespace std::chrono_literals;
 
@@ -22,7 +33,7 @@ static void loop() {
     Json::Value config;
     std::ifstream configFile(configPath, std::ifstream::binary);
     if (!configFile.is_open()) {
-        ABORT_ALWAYS(ref::Format("Failed to open %s", configPath).c_str());
+        ABORT_ALWAYS(ref::Format("Failed to open %s", configPath));
     }
     configFile >> config;
 
@@ -33,29 +44,33 @@ static void loop() {
     ref::WebcamDriver webcam{g.nodes()[0], g};
     ref::Recorder recorder{g.nodes()[1], g};
 
-    // FIXME: Replace all of this with a Controller
-    auto start = ref::Time::NowHiRes();
+    ref::Controller controller{g, CreateNode};
+
+    auto start = ref::Time::Now();
     auto lastTick = start;
+    std::vector<ref::NodeBase*> ready;
+    ref::Time::TimePoint tickDeadline;
+
     do {
-        auto now = ref::Time::NowHiRes();
-        auto elapsed = now - lastTick;
-        if (elapsed >= 10ms) {
-            lastTick = now;
-            elapsed = elapsed % 10ms;
+        auto now = ref::Time::Now();
 
-            webcam.inputs()->setCurrentTime(ref::Time::Now());
-            webcam.tick();
+        controller.readyNodes(now, &ready, &tickDeadline);
 
-            recorder.inputs()->setCurrentTime(ref::Time::Now());
-            recorder.tick();
+        if (ready.size()) {
+            controller.tickNodes(now, ready);
         } else {
-            ref::Time::Nanosleep(10ms - elapsed);
+            // Find the delay in milliseconds between now and the next deadline
+            double delayMS = ref::Time::DeltaMS(now, tickDeadline);
+            // Clamp the delay to [0-1] milliseconds
+            delayMS = std::max(0.0, std::min(1.0, delayMS));
+            // Sleep for this amount of time
+            ref::Time::Nanosleep(uint64_t(delayMS * 1e6));
         }
     } while (lastTick - start < 3s);
 }
 
 int main(int, char const* []) {
     ref::Time::Init();
-    loop();
+    Loop();
     return 0;
 }
